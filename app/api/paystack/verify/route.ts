6ba29999f38e351915714657ce455a32afffe61f
Lawliettt <aderinsolasamashimi@gmail.com>
2026-06-23 15:32:44 +0100

    latest commit

--- app/api/paystack/verify/route.ts
+++ app/api/paystack/verify/route.ts
@@ -24,30 +24,3 @@ export async function GET(req: Request) {
 
     return NextResponse.json(data);
 }
-
-
-
-// import { NextResponse } from "next/server";
-
-// export async function GET(
-//     req: Request
-// ) {
-//     const { searchParams } =
-//         new URL(req.url);
-
-//     const reference =
-//         searchParams.get("reference");
-
-//     const response = await fetch(
-//         `https://api.paystack.co/transaction/verify/${reference}`,
-//         {
-//             headers: {
-//                 Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-//             },
-//         }
-//     );
-
-//     const data = await response.json();
-
-//     return NextResponse.json(data);
-// }
